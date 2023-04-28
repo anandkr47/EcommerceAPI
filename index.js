f8c130b73aacf745160ac8e98df41638423e6dd7
Anand kumar <anandkumar.triden@gmail.com>
2023-04-29 01:07:54 +0530

    initialcommit

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to MongoDBmongoose.set('strictQuery', false);
-mongoose.connect("mongodb://127.0.0.1/api")
-.then(()=>{
-    console.log("mongodb connected");
-})
-.catch((err)=>{
-    console.log(err);
-    console.log("failed to connect");
-})
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed: ", err);
+  });
 
-// Define product schema
 const productSchema = new mongoose.Schema({
   name: String,
   quantity: Number,
 });
 
-// Create product model
 const Product = mongoose.model('Product', productSchema);
 
-// Define routes
 app.get('/api/products', async (req, res) => {
   const products = await Product.find();
   res.json(products);
@@ -40,8 +37,7 @@ app.post('/api/products', async (req, res) => {
 });
 
 app.put('/api/products/:id', async (req, res) => {
-    const product = await Product.findOneAndUpdate({ _id: req.params.id }, { quantity: req.body.quantity }, { new: true });
-
+  const product = await Product.findOneAndUpdate({ _id: req.params.id }, { quantity: req.body.quantity }, { new: true });
   res.json(product);
 });
 
@@ -50,7 +46,6 @@ app.delete('/api/products/:id', async (req, res) => {
   res.json({ message: 'Product deleted' });
 });
 
-// Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
